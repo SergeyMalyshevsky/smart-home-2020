fbb8174bd2b50df34fd5add43c93be7ee4dd0dcb
Sergey Malyshevsky <SMMalyshevskiy@sberbank.ru>
2020-11-05 00:42:07 +0300

    Add unit tests

--- src/test/java/ru/sbt/mipt/oop/signaling/SignalingTest.java
+++ src/test/java/ru/sbt/mipt/oop/signaling/SignalingTest.java
@@ -1,18 +1,53 @@
 package ru.sbt.mipt.oop.signaling;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SignalingTest {
 
-    @org.junit.jupiter.api.Test
-    void activate() {
+    @Test
+    void checkIsAlarmWhenIncorrectCode() {
+        Signaling signaling = new Signaling();
+        String code = "real_code";
+        signaling.activate(code);
+        signaling.deactivate("bad_code");
+        assertTrue(signaling.isAlarmed());
     }
 
-    @org.junit.jupiter.api.Test
-    void deactivate() {
+    @Test
+    void checkIsDeactivateWhenCodeIsCorrect() {
+        Signaling signaling = new Signaling();
+        String code = "real_code";
+        signaling.activate(code);
+        signaling.deactivate(code);
+        assertTrue(signaling.isDeactivated());
     }
 
-    @org.junit.jupiter.api.Test
-    void switchToAlarmMode() {
+    @Test
+    void checkActivate() {
+        Signaling signaling = new Signaling();
+        String code = "real_code";
+        signaling.activate(code);
+        assertTrue(signaling.isActivated());
+    }
+
+    @Test
+    void checkSpecialStopAlarmMethod() {
+        Signaling signaling = new Signaling();
+        String code = "real_code";
+        signaling.activate(code);
+        signaling.deactivate("bad_code");
+        assertTrue(signaling.isAlarmed());
+        signaling.stopAlarm();
+        assertTrue(signaling.isDeactivated());
+    }
+
+    @Test
+    void checkNotAlarmWhenDeactivated() {
+        Signaling signaling = new Signaling();
+        assertTrue(signaling.isDeactivated());
+        signaling.deactivate("test_code");
+        assertTrue(signaling.isDeactivated());
     }
 }
